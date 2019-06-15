369010d359094c142f06f63beaf17f747e85a410
laithmarwan <laithmr1@hotmail.com>
2019-06-15 18:56:44 +0300

    initial

---
+++ app/src/main/java/com/example/icloset/AppInfo.kt
@@ -0,0 +1,18 @@
+package com.example.icloset
+
+
+class AppInfo {
+
+    companion object {
+
+        var web:String="http://paproject.online/"
+
+        var UserID:Int=0
+        var Name:String=""
+        var Email:String=""
+        var Address:String=""
+
+
+    }
+
+}
--- app/src/main/java/com/example/icloset/LoginAct.kt
+++ app/src/main/java/com/example/icloset/LoginAct.kt
@@ -1,7 +1,15 @@
 package com.example.icloset
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginAct : AppCompatActivity() {
 
@@ -9,6 +17,56 @@ class LoginAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        login_new.setOnClickListener {
+
+            var i= Intent(this,NewUserAct::class.java)
+            startActivity(i)
+
+        }
+
+
+        login_button.setOnClickListener {
+
+            var pd= ProgressDialog(this)
+            pd.setMessage("Please Wait...")
+            pd.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+            pd.show()
+
+            var rq= Volley.newRequestQueue(this)
+            var sr=object: StringRequest(
+
+                Request.Method.POST,AppInfo.web+"login.php",
+                Response.Listener { response ->
+                    pd.hide()
+                    if(response=="1") {
+                        AppInfo.Email = login_mobile.text.toString()
+                        var i = Intent(this, MainActivity::class.java)
+                        startActivity(i)
+                        finish()
+                    }
+                    else
+                        Toast.makeText(this,"Login Fail",
+                            Toast.LENGTH_LONG).show()
+                },
+                Response.ErrorListener { error ->
+                    pd.hide()
+                    Toast.makeText(this,error.message,
+                        Toast.LENGTH_LONG).show()
+                })
+            {
+                override fun getParams(): MutableMap<String, String> {
+                    var map=HashMap<String,String>()
+
+                    map.put("Email",login_mobile.text.toString())
+                    map.put("Password",login_password.text.toString())
+
+                    return map
+                }
+            }
+
+            rq.add(sr)
+
+        }
 
     }
 }
