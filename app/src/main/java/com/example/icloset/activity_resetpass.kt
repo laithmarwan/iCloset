4195c2a95049f57c65cb00d310d1a25c0150657e
AwsSafarini <Ironheart_Aws@hotmail.com>
2019-06-17 19:49:03 +0300

    Aws_LoginAct11112

---
+++ app/src/main/java/com/example/icloset/activity_resetpass.kt
@@ -0,0 +1,12 @@
+package com.example.icloset
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_resetpass : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resetpass)
+    }
+}
