83efded05706aff0783d9f74be26a9fdd83c7a8f
laithmarwan <laithmr1@hotmail.com>
2019-06-17 20:41:09 +0300

    Laith1.3 navreadu

--- app/src/main/java/com/example/icloset/MainActivity.kt
+++ app/src/main/java/com/example/icloset/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        main_nav.menu.getItem(2).setChecked(true)
 
 
 
