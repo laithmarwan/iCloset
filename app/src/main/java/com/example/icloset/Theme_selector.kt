f99202dbeb1da4abb8ba387aa6d806ab3f75e848
AwsSafarini <Ironheart_Aws@hotmail.com>
2019-07-01 22:59:26 +0300

    Design stuff

--- app/src/main/java/com/example/icloset/Theme_selector.kt
+++ app/src/main/java/com/example/icloset/Theme_selector.kt
@@ -4,28 +4,45 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.preference.PreferenceManager
+import com.example.icloset.AppInfo.Companion.wait
 import kotlinx.android.synthetic.main.activity_theme_selector.*
 
 class Theme_selector : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        if(AppInfo.theme == 0)
-        setTheme(R.style.AppTheme)
-    else{
-        setTheme(R.style.BlackTheme)
-    }
+        if(AppInfo.theme == 0){
+            setTheme(R.style.AppTheme)
+        }
+
+        if(AppInfo.theme == 1){
+            setTheme(R.style.AppTheme1)
+        }
+
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_theme_selector)
 
-        btn_theme.setOnClickListener {
+        rb_default.setOnClickListener {
+            AppInfo.theme = 0
+            startActivity(Intent(this,Theme_selector::class.java))
+            finish()
+        }
 
+        rb_theme1.setOnClickListener {
             AppInfo.theme = 1
             startActivity(Intent(this,Theme_selector::class.java))
             finish()
         }
 
-
+        button3.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
 
 
     }
